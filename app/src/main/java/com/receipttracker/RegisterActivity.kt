266cb277b77f688c40a4ab35f16d14b01f5961b1
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-23 14:06:19 -0400

    Finished the Register Activity.

--- app/src/main/java/com/receipttracker/RegisterActivity.kt
+++ app/src/main/java/com/receipttracker/RegisterActivity.kt
@@ -2,11 +2,17 @@ package com.receipttracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btn_register_create.setOnClickListener {
+            finish()
+        }
+
     }
 }
