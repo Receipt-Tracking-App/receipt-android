dcbf02e39d52c7b273c727015ff76bf38ece305d
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-23 15:37:13 -0600

    Create ReceiptViewModel.kt
    view model created

---
+++ app/src/main/java/com/receipttracker/ViewModel/ReceiptViewModel.kt
@@ -0,0 +1,25 @@
+package com.receipttracker.ViewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.receipttracker.model.SavedReceipt
+import com.receipttracker.model.repo
+
+class ReceiptViewModel : ViewModel(){
+
+    val receipts: LiveData<List<SavedReceipt>> by lazy {
+
+        readAllReceipts()
+    }
+    fun readAllReceipts() : LiveData<List<SavedReceipt>>{
+
+        return repo.readAllReceipts()
+    }
+    fun updateReceipt(receipt: SavedReceipt){
+        repo.updateReceipt(receipt)
+    }
+    fun deleteReceipt(receipt: SavedReceipt){
+        repo.deleteReceipt(receipt)
+    }
+
+}
