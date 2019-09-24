6cc9d2f9e0d7fbb43f1a920e90ea50234178277f
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-23 23:42:35 -0400

    Added error logic for the login and a dev skip button for testing

--- app/src/main/java/com/receipttracker/LoginActivity.kt
+++ app/src/main/java/com/receipttracker/LoginActivity.kt
@@ -8,21 +8,24 @@ import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    private var validatedUsername: Boolean = false
+    private var validatedPassword: Boolean = false
+
+    //Making these variables global since they're probably gonna be needed when checking the database
+    lateinit var username: String
+    lateinit var password: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        //Move to the list activity.
+        //Move to the list activity if username and password are good
         btn_login.setOnClickListener {
-            //Gets the text from the username edit text
-            var username = text_input_username.editText?.text.toString().trim()
 
-            //Gets the text from the password edit text
-            var password = text_input_password.editText?.text.toString().trim()
-            Toast.makeText(this, "Username: $username\nPassword: $password", Toast.LENGTH_SHORT).show()
+            validateUsername()
+            validatePassword()
+            confirmLogin()
 
-            //If username and password match to a user in the database, move to the list activity
-            startActivity(Intent(this, ListActivity::class.java))
         }
 
         //Move to the register activity when the register button is clicked on
@@ -30,6 +33,57 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this,RegisterActivity::class.java))
         }
 
-
+        //Allows us to move to the ListActivity without a legit account
+        //TODO Remove this on official release
+        btn_dev_skip.setOnClickListener {
+            startActivity(Intent(this, ListActivity::class.java))
+        }
     }
+
+    private fun validateUsername():Boolean{
+        //Gets the text from the username text input layout
+        username = text_input_username.editText?.text.toString().trim()
+
+        if(username.isEmpty()){
+            text_input_username.error = "Field can't be empty"
+            validatedUsername = false
+            return false
+        }
+        else{
+            //Removes the error message if it already exists
+            text_input_username.error = null
+            text_input_username.isErrorEnabled = false
+            validatedUsername = true
+            return true
+        }
+    }
+
+    private fun validatePassword():Boolean{
+        //Gets the text from the password text input layout
+        password = text_input_password.editText?.text.toString().trim()
+
+        if(password.isEmpty()){
+            text_input_password.error = "Field can't be empty"
+            validatedPassword = false
+            return false
+        }
+        else{
+            //Removes the error message if it already exists
+            text_input_password.error = null
+            text_input_password.isErrorEnabled = false
+            validatedPassword = true
+            return true
+        }
+    }
+
+    //Checks to see if both login and password are correct. If so, move to ListActivity.
+    private fun confirmLogin(){
+        //If either the username or password is incorrect, prevent logging in.
+        //For some reason, doing if(!validateUsername() || !validatePassword()) messes up the error message)
+        if (!validatedUsername || !validatedPassword)
+            return
+
+        startActivity(Intent(this, ListActivity::class.java))
+    }
+
 }
