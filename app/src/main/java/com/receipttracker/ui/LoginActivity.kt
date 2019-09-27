35f9406cf6c8e55091d3f9c80eb39cda83faa4e6
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-26 22:47:44 -0400

    Going to ignore the compilation error for now because I can't figure out how to fix it.
    Added retrofit call to Login Activity.

--- app/src/main/java/com/receipttracker/ui/LoginActivity.kt
+++ app/src/main/java/com/receipttracker/ui/LoginActivity.kt
@@ -3,13 +3,21 @@ package com.receipttracker.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.receipttracker.R
+import com.receipttracker.model.LoginResponse
+import com.receipttracker.model.UserLogin
+import com.receipttracker.remote.ServiceBuilder
 import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
     private var validatedUsername: Boolean = false
     private var validatedPassword: Boolean = false
+    private var error: Boolean? = false
 
     //Making these variables global since they're probably gonna be needed when checking the database
     lateinit var username: String
@@ -26,7 +34,6 @@ class LoginActivity : AppCompatActivity() {
             validateUsername()
             validatePassword()
             confirmLogin()
-
         }
 
         //Move to the register activity when the register button is clicked on
@@ -90,13 +97,36 @@ class LoginActivity : AppCompatActivity() {
     }
 
     //Checks to see if both login and password are correct. If so, move to ListActivity.
+    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     private fun confirmLogin() {
         //If either the username or password is incorrect, prevent logging in.
         //For some reason, doing if(!validateUsername() || !validatePassword()) messes up the error message)
         if (!validatedUsername || !validatedPassword)
             return
 
-        startActivity(Intent(this, ListActivity::class.java))
+        val call: Call<LoginResponse> = ServiceBuilder.create().userLoginPost(UserLogin(username, password))
+
+        call.enqueue(object:Callback<LoginResponse>{
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                //Log.i("onFailure", t.message)
+                return
+            }
+
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                error = response.body()?.error
+                Log.i("onResponsee", response.body()?.error.toString())
+
+                if(error == false){
+                    text_input_username.error = null
+                    text_input_password.error = null
+                    startActivity(Intent(this@LoginActivity, ListActivity::class.java))
+                }
+                else{
+                    text_input_username.error = "Either Invalid Username or Password"
+                    text_input_password.error = "Either Invalid Username or Password"
+                }
+            }
+        })
     }
 
     //Removes the error messages when moving into the login activity
@@ -110,3 +140,23 @@ class LoginActivity : AppCompatActivity() {
         super.onPostResume()
     }
 }
+/*
+        val call: Call<ReceiptResponse> = ServiceBuilder.create().createNewReceipt(token,
+            Receipt(date, merchant, cost, description))
+
+
+        call.enqueue(object: Callback<ReceiptResponse>{
+            override fun onFailure(call: Call<ReceiptResponse>, t: Throwable) {
+                Log.i("onFailure", t?.message)
+            }
+
+            override fun onResponse(
+                call: Call<ReceiptResponse>,
+                response: Response<ReceiptResponse>
+            ) {
+                val message = response.body()!!.message
+                Log.i("onResponse", message)
+            }
+
+        })
+ */
