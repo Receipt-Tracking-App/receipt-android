bfd4042d398400bd9810cc368d4d4aaec9fa842b
Vdtyson <VersilisTyson@gmail.com>
2019-09-27 14:35:36 -0400

    Adds method isValid() to data class ValidationWithMessage

--- app/src/main/java/com/receipttracker/ui/ReceiptRecyclerViewAdapter.kt
+++ app/src/main/java/com/receipttracker/ui/ReceiptRecyclerViewAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isGone
 import androidx.recyclerview.widget.RecyclerView
@@ -13,10 +14,11 @@ import com.receipttracker.R
 import com.receipttracker.model.SavedReceipt
 import kotlinx.android.synthetic.main.recycler_item_receipt.view.*
 
-class ReceiptRecyclerViewAdapter(private var savedReceiptList: List<SavedReceipt>): RecyclerView.Adapter<ReceiptRecyclerViewAdapter.ViewHolder>() {
+class ReceiptRecyclerViewAdapter(private var savedReceiptList: MutableList<SavedReceipt>): RecyclerView.Adapter<ReceiptRecyclerViewAdapter.ViewHolder>() {
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+        val cardView = view.item_cv as CardView
         val priceTextView = view.recyclerItemReceipt_priceTV as TextView
         val dateTextView = view.recyclerItemReceipt_dateTV as TextView
         val businessNameTextView = view.recyclerItemReceipt_businessTV as TextView
@@ -27,6 +29,11 @@ class ReceiptRecyclerViewAdapter(private var savedReceiptList: List<SavedReceipt
         val expandableLinearLayout = view.recyclerItemReceipt_expandableCL as ConstraintLayout
         val showReceiptPictureBttn = view.recyclerItemReceipt_expandedShowReceiptBttn as MaterialButton
         val notesTextView = view.recyclerItemReceipt_notesTV as TextView
+
+        fun cardViewDeleteOnLongPress(itemPosition: Int) {
+            ListActivity.receiptList.removeAt(itemPosition)
+            updateRV(ListActivity.receiptList)
+        }
 
         fun expandAndCollapseBttnOnClick() {
 
@@ -49,13 +56,18 @@ class ReceiptRecyclerViewAdapter(private var savedReceiptList: List<SavedReceipt
     }
 
     override fun getItemCount(): Int {
-        return savedReceiptList.size
+        return savedReceiptList.size -1
     }
 
     override fun onBindViewHolder(holder: ReceiptRecyclerViewAdapter.ViewHolder, position: Int) {
 
         holder.notesTextView.text = savedReceiptList[position].notes
         holder.priceTextView.text = savedReceiptList[position].amount.toString()
+
+        holder.priceTextView.setOnLongClickListener {
+            holder.cardViewDeleteOnLongPress(position)
+            return@setOnLongClickListener true
+        }
 
         var date = savedReceiptList[position].purchaseDate
         val split = date.split(":")
@@ -71,7 +83,7 @@ class ReceiptRecyclerViewAdapter(private var savedReceiptList: List<SavedReceipt
         }
     }
 
-    fun updateRV(newList: List<SavedReceipt>) {
+    fun updateRV(newList: MutableList<SavedReceipt>) {
         savedReceiptList = newList
         notifyDataSetChanged()
     }
